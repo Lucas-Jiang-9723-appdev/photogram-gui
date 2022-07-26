d6e806a237f28699a682ffcfc642b5dc7396c7a6
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-26 01:19:55 +0000

    2nd commit - lack actual working CU function

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,24 @@
+class PhotosController < ApplicationController
+  def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({:created_at => :desc})
+    render({:template => "photo_templates/index.html.erb"})
+  end
+
+  def show
+    url_id = params.fetch("path_photo")
+    matching_photos = Photo.where({:id=>url_id})
+    @the_photo = matching_photos.first
+    render({:template => "photo_templates/show.html.erb"})
+  end
+
+  def delete
+    delete_id = params.fetch("delete_id")
+    matching_photos = Photo.where({:id => delete_id})
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+
+    # render({:template => "photo_templates/delete.html.erb"})
+    redirect_to("/photos")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:path_username", {:controller => "users", :action => "show"})
+  get("/photos", {:controller => "photos", :action => "index"})
+  get("/photos/:path_photo", {:controller => "photos", :action => "show"})
+  get("/delete_photo/:delete_id", {:controller => "photos", :action => "delete"})
+  get("/", {:controller => "users", :action => "index"})
   # get("", {:controller => "", :action => ""})
 
 end
